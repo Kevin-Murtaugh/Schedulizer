a5c5a3e184538b1d7495672f276c70ad74b27bf0
dkayucf <dkayucf@gmail.com>
2018-05-10 18:03:00 -0400

    added ADD SHIFT buttons to calendar and set up them to the correct route

--- public/js/appFrontEnd.js
+++ public/js/appFrontEnd.js
@@ -1,4 +1,3 @@
-$(function() {
 
   $('#calendar').fullCalendar({
     themeSystem: 'bootstrap4',
@@ -14,7 +13,106 @@ $(function() {
     eventLimit: true // allow "more" link when too many events
   });
 
-});
+
+
+//===============ITEM CONTROLLER==================
+const ItemCtrl = (function(){
+
+    //Public Methods
+    return {
+
+    }
+
+})();
+
+
+
+//================UI CONTROLLER==================
+const UICtrl = (function(){
+    
+    const UISelectors = {
+        //Buttons
+        addShiftBtn: '.addShiftBtn',
+        
+        //Inputs
+        
+        //Selects
+        
+        //TextAreas
+        
+        //Calendar
+        dayHeader: '.fc-day-header',
+        nextWeek: '.fc-next-button',
+        prevWeek: '.fc-prev-button',
+        
+        //Table
+        tableBordered: '.table-bordered'
+        
+        
+    }
+    
+    //Public Methods
+    return {
+        getSelectors: () => {
+            return UISelectors;
+        }
+    }
+
+})();
+
+
+
+//==============APP CONTROLLER=================
+const AppCtrl = (function(ItemCtrl, UICtrl){
+    //Get UI selectors
+    const UISelectors = UICtrl.getSelectors();
+    
+    const loadEventListeners = ()=>{
+        /*----------------INPUT Events-----------------*/
+        
+        /*----------------CLICK Events-----------------*/
+        document.querySelector(UISelectors.nextWeek).addEventListener('click', appendAddShiftBtns);
+        document.querySelector(UISelectors.prevWeek).addEventListener('click', appendAddShiftBtns);
+              
+        document.querySelector(UISelectors.tableBordered).addEventListener('click', (e)=>{
+            if(e.target.classList.contains('addShiftBtn')){
+                let date = e.target.getAttribute('data-date');
+                location.href = `/dashboard/add-shift/${date}`;
+            }
+            
+        });
+        
+        
+        /*----------------CHANGE Events-----------------*/
+        
+    }
+    
+    const appendAddShiftBtns = function(){
+        let headerDays = Array.from(document.querySelectorAll(UISelectors.dayHeader));
+        
+        headerDays.forEach(day=>{
+            let date = day.getAttribute('data-date');
+
+            let addShiftBtn = document.createElement('button');
+                addShiftBtn.classList = 'btn btn-secondary addShiftBtn';
+                addShiftBtn.innerHTML = 'Add Shift';
+                addShiftBtn.setAttribute('data-date', date);
+            day.appendChild(addShiftBtn);
+        })
+    }
+    
+    //Public Methods
+    return {
+        init: () => {
+            appendAddShiftBtns();
+            loadEventListeners();
+            
+        }
+    }
+
+})(ItemCtrl, UICtrl);
+
+AppCtrl.init();
         
         
         
