8dbb095385c51f95be9b32bd33a4dd0a848781ab
Joshua Arnett <Josharnett850@gmail.com>
2018-05-10 11:41:05 -0400

    created josh config option & created dashboard route

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,3 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const {ensureAuthenticated, ensureGuest} = require('../helpers/auth');
+
+const db = require("../models");
+
+
+router.get("/dashboard", function(req, res) {
+    res.render("index/dashboard");
+});
+
+
